ddcc1d369c81d8e7d241712b9f301618c834e157
Blizihguh <blizihguh@gmail.com>
2021-06-03 13:53:31 -0400

    Updated Template.lua with more boilerplate code

--- Template.lua
+++ Template.lua
@@ -2,11 +2,17 @@ local games = require("Games")
 local misc = require("Misc")
 local REPLACE_GAME_NAME = {}
 
+--IMPORTANT: Declare all function names (besides the startGame/commandHandler/dmHandler/optional reactHandler functions) as local variables here.
+--           If you don't do this, they will be imported as global functions, possibly disrupting other games!
+local quitGame
+
 --#############################################################################################################################################
 --# Main Functions                                                                                                                            #
 --#############################################################################################################################################
 
 function REPLACE_GAME_NAME.startGame(message)
+	-- Use the name as you formatted it in MayorMcMott.lua's GAME_LIST table
+	--games.registerGame(message.channel, "GameName", state, message.mentionedUsers)
 end
 
 function REPLACE_GAME_NAME.commandHandler(message, state)
@@ -22,5 +28,10 @@ end
 --#############################################################################################################################################
 --# Game Functions                                                                                                                            #
 --#############################################################################################################################################
+
+function quitGame(state)
+	state.GameChannel:send("Quitting game...")
+	games.deregisterGame(state.GameChannel)	
+end
 
 return REPLACE_GAME_NAME
