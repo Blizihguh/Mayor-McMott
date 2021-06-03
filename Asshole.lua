8599a2d4b92687a61bf60015ffa98fa0a4c86843
Blizihguh <blizihguh@gmail.com>
2021-06-03 13:43:40 -0400

    Fully implemented The Asshole Game

--- Asshole.lua
+++ Asshole.lua
@@ -2,7 +2,7 @@ local games = require("Games")
 local misc = require("Misc")
 local asshole = {}
 
-local status
+local status, point, quitGame, checkForEnd
 
 --#############################################################################################################################################
 --# Main Functions                                                                                                                            #
@@ -18,7 +18,7 @@ function asshole.startGame(message)
 	local cards = misc.shuffleTable({"King", "King", "Jack", "Jack", "Asshole", "Asshole", "Jester"})
 	local idx = 1
 	for id,playerObject in pairs(message.mentionedUsers) do
-		playerlist[idx] = {Player = playerObject, Card = cards[idx], Pointed = false}
+		playerlist[idx] = {Player = playerObject, Card = cards[idx], Pointed = false, Won = false, StatusID = nil}
 		idx = idx + 1
 	end
 
@@ -35,18 +35,134 @@ function asshole.startGame(message)
 end
 
 function asshole.commandHandler(message, state)
+	local args = message.content:split(" ")
+	if args[1] == "!quit" then quitGame(state) end
 end
 
 function asshole.dmHandler(message, state)
 end
 
 function asshole.reactHandler(reaction, user, state)
-	print("Hello!")
+	-- Check if the message is the user's status message
+	for idx,playerObject in pairs(state.PlayerList) do
+		-- Check if the reaction was made by the user (not the bot) and is on the status message and the user hasn't pointed yet
+		if playerObject.Pointed == false and playerObject.Player.id == user.id and playerObject.StatusID == reaction.message.id then
+			-- The user has selected a number!
+			point(state, user, reaction.emojiName)
+		end
+	end
 end
 
 --#############################################################################################################################################
 --# Game Functions                                                                                                                            #
 --#############################################################################################################################################
+
+function quitGame(state)
+	state.GameChannel:send("Quitting game...")
+	games.deregisterGame(state.GameChannel)
+end
+
+function point(state, user, emoji)
+	-- Get target number in a reasonable format
+	local emojiTable = {["0️⃣"] = 0, ["1️⃣"] = 1, ["2️⃣"] = 2, ["3️⃣"] = 3, ["4️⃣"] = 4}
+	local target_number = emojiTable[emoji]
+
+	-- Get player info
+	local my_number = 0
+	local player = nil
+	for idx,playerObject in pairs(state.PlayerList) do
+		if playerObject.Player.id == user.id then
+			my_number = idx
+			player = playerObject
+			break
+		end
+	end
+
+	-- Set our win status (and return if we aren't actually pointing!)
+	if target_number == nil then return
+	elseif target_number == 0 then
+		-- Win if we're the only King or the only Jack
+		if state.PlayerList[my_number].Card == "King" or state.PlayerList[my_number].Card == "Jack" then
+			local won = true
+			for idx,playerObject in pairs(state.PlayerList) do
+				if idx ~= my_number and playerObject == state.PlayerList[my_number].Card then
+					won = false
+					break
+				end
+			end
+			state.PlayerList[my_number].Won = won
+		end
+	else
+		-- Win if the target player shares our card, as long as it's not a non-jester self-pointing
+		if state.PlayerList[my_number].Card == state.PlayerList[target_number].Card then
+			if state.PlayerList[my_number].Card == "Jester" or my_number ~= target_number then
+				state.PlayerList[my_number].Won = true
+			end
+		end
+		-- If target player is an asshole and we lose by pointing at them, they win
+		if state.PlayerList[target_number].Card == "Asshole" and state.PlayerList[my_number].Won == false then
+			state.PlayerList[target_number].Won = true
+		end
+	end
+
+	-- Set our point status
+	state.PlayerList[my_number].Pointed = true
+
+	-- Broadcast the point
+	local roleEmojis = {King = ":large_blue_diamond:", Jack = ":red_circle:", Asshole = ":green_heart:", Jester = ":warning:", Self = ":grey_question:", Nobody = ":white_square_button:"}
+	local numbers = {[0] = ":zero:", [1] = ":one:", [2] = ":two:", [3] = ":three:", [4] = ":four:"}
+	local target_card = ":white_square_button:"
+	local target_name = "Nobody"
+	if target_number ~= 0 then 
+		target_card = roleEmojis[state.PlayerList[target_number].Card]
+		target_name = state.PlayerList[target_number].Player.name 
+	end
+	local pointString = string.format("%s%s **%s** points at %s%s **%s**!", roleEmojis[player.Card], numbers[my_number], user.name, target_card, emoji, target_name)
+
+	for idx,playerObject in pairs(state.PlayerList) do
+		if idx == target_number and target_number ~= my_number and not playerObject.Pointed then
+			playerObject.Player:send(string.format("%s%s **%s** points at %s%s **%s**!", roleEmojis[player.Card], numbers[my_number], user.name, ":grey_question:", emoji, target_name))
+		else
+			playerObject.Player:send(pointString)
+		end
+	end
+
+	-- Check for the end of the game
+	checkForEnd(state)
+end
+
+function checkForEnd(state)
+	-- Get game status
+	local jesterWin = false
+	local winningPlayers = {}
+	local winnerCount = 0
+	local pointCount = 0
+
+	for idx,player in pairs(state.PlayerList) do
+		if player.Card == "Jester" and player.Won then
+			jesterWin = true
+			winnerCount = 1
+			winningPlayers = {}
+			table.insert(winningPlayers, player.Player.name)
+			break
+		end
+		if player.Won then 
+			winnerCount = winnerCount + 1
+			table.insert(winningPlayers, player.Player.name)
+		end
+		if player.Pointed then pointCount = pointCount + 1 end
+	end
+
+	-- Handle game overs
+	local winStrings = {[0] = "**Game over!** Everybody lost! :clap::clap::clap:", [1] = "**Game over!** %s won!", [2] = "**Game over!** %s and %s won!", [3] = "**Game over!** %s, %s, and %s won!", [4] = "**Game over!** Everybody won! ...wait, what? :flushed:"}
+	if jesterWin or pointCount == 4 or winnerCount >= 2 then
+		local output = string.format(winStrings[winnerCount], winningPlayers[1], winningPlayers[2], winningPlayers[3], winningPlayers[4])
+		for idx,playerObject in pairs(state.PlayerList) do
+			playerObject.Player:send(output)
+		end
+		quitGame(state)
+	end
+end
 
 function status(state, target)
 	local border_string = "+===============================+\n"
@@ -70,6 +186,11 @@ function status(state, target)
 	message:addReaction("2️⃣")
 	message:addReaction("3️⃣")
 	message:addReaction("4️⃣")
+	for idx,playerObject in pairs(state.PlayerList) do
+		if playerObject.Player.id == target.Player.id then
+			state.PlayerList[idx].StatusID = message.id
+		end
+	end
 end
 
 return asshole
