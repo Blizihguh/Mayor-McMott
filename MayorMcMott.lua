Blizihguh/Mayor-McMott

commit d13bc36755e77b5333fdee20c835e8019d9bc0e1

Close #33

MayorMcMott.lua
@@ -62,7 +62,7 @@ function gameCommands(message)
 	local author = message.author
 	local args = content:split(" ")
 
-	if misc.keyInTable(channel, games.INSTANCES) then -- Channel has game already
+	if misc.keyInTable(channel, games.INSTANCES) and (GAME_LIST[gameType].commandHandler ~= nil) then -- Channel has game already
 		-- Run game-specific functions
 		local gameType = games.INSTANCES[channel][2]
 		local state = games.INSTANCES[channel][3]

plugins/Chameleon.lua
@@ -199,14 +199,6 @@ function chameleon.startGame(message, playerList)
 	end
 end
 
-function chameleon.commandHandler(message, state)
-	local args = message.content:split(" ")
-	if args[1] == "!quit" then
-		games.deregisterGame(state["GameChannel"])
-		message.channel:send("Quiting game...")
-	end
-end
-
 --#############################################################################################################################################
 --# Game Functions                                                                                                                            #
 --#############################################################################################################################################

plugins/Conspiracy.lua
@@ -19,9 +19,6 @@ function conspiracy.startGame(message, players)
     end
 end
 
-function conspiracy.commandHandler(message, state)
-end
-
 --#############################################################################################################################################
 --# Game Functions                                                                                                                            #
 --#############################################################################################################################################

plugins/Mafia.lua
@@ -24,9 +24,6 @@ function mafia.startGame(message, playerList)
     end
 end
 
-function mafia.commandHandler(message, state)
-end
-
 --#############################################################################################################################################
 --# Game Functions                                                                                                                            #
 --#############################################################################################################################################

commit aed2124e5ef48a7c7ada697c54a22a2b39e4d324

Fixed crash introduced by d13bc36

MayorMcMott.lua
@@ -62,9 +62,10 @@ function gameCommands(message)
 	local author = message.author
 	local args = content:split(" ")
 
-	if misc.keyInTable(channel, games.INSTANCES) and (GAME_LIST[gameType].commandHandler ~= nil) then -- Channel has game already
+	if misc.keyInTable(channel, games.INSTANCES) then -- Channel has game already
 		-- Run game-specific functions
 		local gameType = games.INSTANCES[channel][2]
+		if GAME_LIST[gameType].commandHandler == nil then return end
 		local state = games.INSTANCES[channel][3]
 		local stat, err, ret = xpcall(GAME_LIST[gameType].commandHandler, debug.traceback, message, state)
 		if not stat then