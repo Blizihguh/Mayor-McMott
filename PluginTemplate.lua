193dec925c43ced095f62b578faf2176d648d517
Blizihguh <blizihguh@gmail.com>
2022-11-16 15:21:52 -0600

    Revert "Updated plugin templates with new channel syntax"
    This reverts commit 38730a312055e3ba4bd52e642aaeeb20cd5899da.

--- PluginTemplate.lua
+++ PluginTemplate.lua
@@ -31,7 +31,7 @@ local quitGame
 --# Main Functions                                                                                                                            #
 --#############################################################################################################################################
 
-function REPLACE_GAME_NAME.startGame(message, playerList, channel)
+function REPLACE_GAME_NAME.startGame(message, playerList)
 	-- args[1] will be !start/!vc, and args[2] will be the game name.
 	-- The actual arguments, if the game takes them, will start at args[3]
 	local args = message.content:split(" ")
@@ -40,13 +40,13 @@ function REPLACE_GAME_NAME.startGame(message, playerList, channel)
 	-- When you finish initializing the game, you save the state table with games.registerGame (see below)
 	-- When players call commands, a reference to this table will be passed to the command handler, so that you can use or modify the game's state
 	local state = {
-		GameChannel = channel,
+		GameChannel = message.channel,
 		PlayerList = playerList -- You may want to create a new list with more information tracked per player, and save that to state
 	}
 
 	-- Use the name as it appears in MayorMcMott.lua's GAME_LIST table, and the same player list from the function args (don't modify it!)
 	-- For very simple games that don't need commands (eg Chameleon), just delete this line, as well as the quitGame function
-	--state.GameID = games.registerGame(channel, "GameName", state, playerList)
+	--state.GameID = games.registerGame(message.channel, "GameName", state, playerList)
 end
 
 function REPLACE_GAME_NAME.commandHandler(message, state)
